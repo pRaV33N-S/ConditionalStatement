e31c540a15799014f39f5bd2cd55d1fb6927a8e4
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-13 09:43:47 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int pre = int.Parse(Console.ReadLine());
+            if (pre > 60)
+            { 
+                Console.WriteLine("Selected");
+            }
+            else
+            {
+                Console.WriteLine("Fail in Pre");
+            }
+
+
+        }
+    }
+}
