f284c68c57010758cb5a8b416e703f70d0258256
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-13 09:47:19 +0530

    final commit

--- Program.cs
+++ Program.cs
@@ -10,18 +10,21 @@ namespace ConditionalStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number");
+            Console.WriteLine("Enter the pre mark");
             int pre = int.Parse(Console.ReadLine());
             if (pre > 60)
-            { 
-                Console.WriteLine("Selected");
+            {
+                Console.WriteLine("Enter the final mark");
+                int final = int.Parse(Console.ReadLine());
+                if(final>55)
+                    Console.WriteLine("Selected");
+                else
+                    Console.WriteLine("Fail in Final");
             }
             else
             {
                 Console.WriteLine("Fail in Pre");
             }
-
-
         }
     }
 }
